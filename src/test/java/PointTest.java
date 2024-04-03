eceace7ac73af8e3488e2b5ae8c4503af2c84c11
Momcilo Krunic <momcilo.krunic@labsoft.dev>
2024-04-03 18:21:54 +0200

    #1 Promenjen naziv testa.

--- src/test/java/PointTest.java
+++ src/test/java/PointTest.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 
 public class PointTest {
     @Test
-    public void test() {
+    public void test_getXTrebaDaVrati5AkoJeKlasaInstanciranaSaXjednako5() {
         Point test = new Point(5, 5);
         assertEquals(test.getX(), 5);
     }
